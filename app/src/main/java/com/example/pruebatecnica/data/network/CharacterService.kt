eaede71e704d420fe0e6b1bfdf813d3180ccd8ca
villamizar21 <arleyvillamizar@wposs.com>
2022-07-06 13:54:32 -0500

    se crea el metodo que hace llamado a la Api con el modulo inyectado

---
+++ app/src/main/java/com/example/pruebatecnica/data/network/CharacterService.kt
@@ -0,0 +1,16 @@
+package com.example.pruebatecnica.data.network
+
+import com.example.pruebatecnica.data.api.ApiService
+import com.example.pruebatecnica.data.model.Characters
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class CharacterService @Inject constructor(private val api: ApiService) {
+
+    suspend fun getAllCharacters(): Characters {
+        return withContext(Dispatchers.IO) {
+            api.getCharater()
+        }
+    }
+}
