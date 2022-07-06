3ae3fc1029d449bacf92f1ac69f204ac705ace71
villamizar21 <arleyvillamizar@wposs.com>
2022-07-06 13:19:29 -0500

    Implementacion -se agrega la interfaz para las peticions a la API -se agrega el modulo proveer retrofit y que se pueda inyectar la clase ApiService

---
+++ app/src/main/java/com/example/pruebatecnica/di/NetworkProvider.kt
@@ -0,0 +1,31 @@
+package com.example.pruebatecnica.di
+
+import com.example.pruebatecnica.data.api.ApiService
+import com.example.pruebatecnica.utils.Constans
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkProvider {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(Constans.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService{
+       return retrofit.create(ApiService::class.java)
+    }
+}
