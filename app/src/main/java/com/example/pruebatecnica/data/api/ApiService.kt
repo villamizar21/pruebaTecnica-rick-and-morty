bdc2a9a7dff90d1124a4a0a2eaa68edbeb697f8d
villamizar21 <arleyvillamizar@wposs.com>
2022-07-06 20:31:13 -0500

    Se agrega metodo para hacer el llamado a la Api

--- app/src/main/java/com/example/pruebatecnica/data/api/ApiService.kt
+++ app/src/main/java/com/example/pruebatecnica/data/api/ApiService.kt
@@ -1,10 +1,15 @@
 package com.example.pruebatecnica.data.api
 
+import com.example.pruebatecnica.data.model.CharacterId.CharacterId
 import com.example.pruebatecnica.data.model.Characters.Characters
 import com.example.pruebatecnica.utils.Constans
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
     @GET(Constans.CHARACTER)
     suspend fun getCharater(): Characters
+
+    @GET(Constans.CHARACTER+"/{id}")
+    suspend fun getCharaterId(@Path("id")id:Long): CharacterId
 }
